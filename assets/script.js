3225f8618862c781ad6c654200d2b29ffca0b102
Grant Pardington <grant.pardington@gmail.com>
2020-07-19 20:30:18 -0600

    Completley changed JavaScript and parts of HTML. Now the JavaScript contains the actual quiz content.

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,5 @@
 (function(){
     // Functions
-
     function buildQuiz(){
       // variable to store the HTML output
       const output = [];
@@ -58,8 +57,8 @@
         // if answer is correct
         if(userAnswer === currentQuestion.correctAnswer){
           // add to the number of correct answers
-          numCorrect++;}
-    
+          numCorrect++;
+        }
       });
   
       // show number of correct answers out of total
@@ -70,6 +69,12 @@
       slides[currentSlide].classList.remove('active-slide');
       slides[n].classList.add('active-slide');
       currentSlide = n;
+      if(currentSlide === 0){
+        previousButton.style.display = 'none';
+      }
+      else{
+        previousButton.style.display = 'inline-block';
+      }
       if(currentSlide === slides.length-1){
         nextButton.style.display = 'none';
         submitButton.style.display = 'inline-block';
@@ -83,7 +88,10 @@
     function showNextSlide() {
       showSlide(currentSlide + 1);
     }
-  
+
+    function showPreviousSlide() {
+      showSlide(currentSlide - 1);
+    }
   
     // Variables
     const quizContainer = document.getElementById('quiz');
@@ -160,14 +168,13 @@
         },
         correctAnswer: "c"
     }
-
 ];
   
     // Kick things off
-    
     buildQuiz();
   
     // Pagination
+    const previousButton = document.getElementById("previous");
     const nextButton = document.getElementById("next");
     const slides = document.querySelectorAll(".slide");
     let currentSlide = 0;
@@ -176,7 +183,7 @@
     showSlide(currentSlide);
   
     // Event listeners
+    previousButton.addEventListener("click", showPreviousSlide);
     submitButton.addEventListener('click', showResults);
     nextButton.addEventListener("click", showNextSlide);
   })();
-  
